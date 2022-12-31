fcd192a09dd682974b68c3f9bca1da58cea35aa9
Quentin <Quitar@ymail.com>
2022-12-31 17:41:18 -0600

    Pic Compression, Raid Script Update
    - Compressed a lot of pictures
    - Added combatFade Scripting to Raids
    - Added a heightFade script for W5's SoD markers
    - A few raid category changes
    - Moved some Weekly CM markers around

--- Blish HUD/pack.lua
+++ Blish HUD/pack.lua
@@ -12,12 +12,14 @@ if(Mumble.CurrentMap.Id == 1428) then
 end
 
 --For use with the script-filter attribute
-function markerCombatFade(marker)
-    return Mumble.PlayerCharacter.IsInCombat
-end
-function trailCombatFade(trail)
+function combatFade(marker)
     return Mumble.PlayerCharacter.IsInCombat
 end
 
-
-
+--This is for W5's Statue of Darkness's Light Throwing Spawn Markers
+--so they disappear when on Dhuum's arena
+function heightFade(marker)
+    if(Mumble.PlayerCharacter.Position.Z > marker.Position.Z+30) then
+        return true
+    end
+end
