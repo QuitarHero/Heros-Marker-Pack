c60cf86385abed773a5cef2adcf246de86cce5b9
Quentin <Quitar@ymail.com>
2023-01-08 12:08:36 -0600

    various pre-release fixes

--- Blish HUD/pack.lua
+++ Blish HUD/pack.lua
@@ -1,16 +1,6 @@
 HSP = {}
 HSP = {}
 
---Harvest Temple Scripts
-if(Mumble.CurrentMap.Id == 1437) then
-    Pack:Require("scripts/revHammer.lua")
-end
-
---Arborstone Scripts
-if(Mumble.CurrentMap.Id == 1428) then
-    Pack:Require("scripts/strikeSchedule.lua")
-end
-
 --For use with the script-filter attribute
 function combatFade(marker)
     return Mumble.PlayerCharacter.IsInCombat
