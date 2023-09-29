5992b8e64888be19c3c7a717466fbb94bdc4dbab
Quentin <Quitar@ymail.com>
2023-09-29 13:56:48 -0500

    Some code cleanup

--- Blish HUD/scripts/General/StrB10_D2_SafeSpot.lua
+++ Blish HUD/scripts/General/StrB10_D2_SafeSpot.lua
@@ -3,70 +3,54 @@ HMP.StrB10_PrimordusSafeSpot = {
   center = I:Vector3(15.555, -554.495, 391.713),
   color = { I:Color(64, 255, 54, 255), I:Color(255, 54, 54, 255), I:Color(255, 255, 0, 255) },
   catCheck = { false, false, false, false },
-  textures = {
-    "SafeSpot_300r_and_Edge", "SafeSpot_Max_and_Edge",
-    "SafeSpot_300r", "SafeSpot_Max"
-  },
+  categories = { "HMP.strikeB10_c1.sc1", "HMP.strikeB10_c1.sc1.asc1", "HMP.strikeB10_c1.sc1.asc3", "HMP.strikeB10_c1.sc2" },
+  textures = { "SafeSpot_300r_and_Edge", "SafeSpot_Max_and_Edge", "SafeSpot_300r", "SafeSpot_Max" },
 }
 
-local SafeSpot, player = HMP.StrB10_PrimordusSafeSpot, Mumble.PlayerCharacter
+local SS, player = HMP.StrB10_PrimordusSafeSpot, Mumble.PlayerCharacter
 
 local function UpdateTexture()
-  local getCatStates = {
-    World:CategoryByType("HMP.strikeB10_c1.sc1"):IsVisible(), World:CategoryByType("HMP.strikeB10_c1.sc1.asc1"):IsVisible(),
-    World:CategoryByType("HMP.strikeB10_c1.sc1.asc3"):IsVisible(), World:CategoryByType("HMP.strikeB10_c1.sc2"):IsVisible()
-  }
-  for i = 1, #SafeSpot.catCheck do
-    SafeSpot.catCheck[i] = getCatStates[i]
-  end
+  for i = 1, #SS.catCheck do SS.catCheck[i] = World:CategoryByType(SS.categories[i]):IsVisible() end
   
-  if( SafeSpot.catCheck[1] == false or SafeSpot.catCheck[2] == false ) then
-    SafeSpot.marker:SetTexture("Data/General/empty.png")
+  --No Max Melee or No Boundary
+  if( SS.catCheck[1] == false or SS.catCheck[2] == false ) then
+    SS.marker:SetTexture("Data/General/empty.png")
   else
-    SafeSpot.marker.InGameVisibility = true
-    if( SafeSpot.catCheck[3] == false and SafeSpot.catCheck[4] == false ) then
-      SafeSpot.marker:SetTexture("Data/Shapes/" .. SafeSpot.textures[4] .. ".png")
-    elseif( SafeSpot.catCheck[3] == true and SafeSpot.catCheck[4] == false ) then
-      SafeSpot.marker:SetTexture("Data/Shapes/" .. SafeSpot.textures[3] .. ".png")
-    elseif( SafeSpot.catCheck[3] == false and SafeSpot.catCheck[4] == true ) then
-      SafeSpot.marker:SetTexture("Data/Shapes/" .. SafeSpot.textures[2] .. ".png")
-    else
-      SafeSpot.marker:SetTexture("Data/Shapes/" .. SafeSpot.textures[1] .. ".png")
-    end
+    --No 300r and No Edge
+    if(     SS.catCheck[3] == false and SS.catCheck[4] == false ) then SS.marker:SetTexture("Data/Shapes/" .. SS.textures[4] .. ".png")
+    --Yes 300r and No Edge
+    elseif( SS.catCheck[3] == true  and SS.catCheck[4] == false ) then SS.marker:SetTexture("Data/Shapes/" .. SS.textures[3] .. ".png")
+    --No 300r and Yes Edge
+    elseif( SS.catCheck[3] == false and SS.catCheck[4] == true )  then SS.marker:SetTexture("Data/Shapes/" .. SS.textures[2] .. ".png")
+    --Yes 300r and Yes Edge
+    else SS.marker:SetTexture("Data/Shapes/" .. SS.textures[1] .. ".png") end
   end
 end
 
-UpdateTexture()
+UpdateTexture() --This ensures the marker always has a texture
 
 local function Tick_Primordus_SafeSpot(gameTime)
-  Debug:Watch("Vector", (player.Position - SafeSpot.center ):Length() )
+  Debug:Watch("Vector", (player.Position - SS.center ):Length() )
   
-  if( World:CategoryByType("HMP.strikeB10_c1.sc1"):IsVisible() ~= SafeSpot.catCheck[1] ) then
-    Debug:Print("1")
-    UpdateTexture()
-  elseif( World:CategoryByType("HMP.strikeB10_c1.sc1.asc1"):IsVisible() ~= SafeSpot.catCheck[2] ) then
-    Debug:Print("2")
-    UpdateTexture()
-  elseif( World:CategoryByType("HMP.strikeB10_c1.sc1.asc3"):IsVisible() ~= SafeSpot.catCheck[3] ) then
-    UpdateTexture()
-   elseif( World:CategoryByType("HMP.strikeB10_c1.sc2"):IsVisible() ~= SafeSpot.catCheck[4] ) then
-    UpdateTexture()
-  end
+  --If the user changes their category settings, we change the texture displayed
+  for i = 1, #SS.catCheck do if( World:CategoryByType(SS.categories[i]):IsVisible() ~= SS.catCheck[i] ) then UpdateTexture() end end
   
-  if( ( player.Position - SafeSpot.marker.Position ):Length() > SafeSpot.marker.Size + 0.1) then
-    if( ( player.Position - SafeSpot.center ):Length() <= 14.44 ) then
-      SafeSpot.marker.Tint = SafeSpot.color[1]
+  --Depending on the player's distance from the hitbox and Primy's Lava Slam, we change the texture's color
+  if( ( player.Position - SS.marker.Position ):Length() > SS.marker.Size + 0.1) then
+    if( ( player.Position - SS.center ):Length() <= 14.44 ) then
+      SS.marker.Tint = SS.color[1]
     else
-      SafeSpot.marker.Tint = SafeSpot.color[3]
+      SS.marker.Tint = SS.color[3]
     end
-  elseif( ( player.Position - SafeSpot.marker.Position ):Length() <= SafeSpot.marker.Size + 0.1) then
-    SafeSpot.marker.Tint = SafeSpot.color[2]
+  elseif( ( player.Position - SS.marker.Position ):Length() <= SS.marker.Size + 0.1) then
+    SS.marker.Tint = SS.color[2]
   end
   
+  --When the player moves too far away, we hide the marker
   if( player.Position.X > 19 and player.Position.X < 34 and player.Position.Y < -541 ) then
-    SafeSpot.marker.InGameVisibility = true
+    SS.marker.InGameVisibility = true
   else
-    SafeSpot.marker.InGameVisibility = false
+    SS.marker.InGameVisibility = false
   end
 end
 
