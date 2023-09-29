500ed6a897847b9b268cd4a63315ffcf2fb0939b
Quentin <Quitar@ymail.com>
2023-09-29 12:28:57 -0500

    Added Max Melee Check & Color

--- Blish HUD/scripts/General/StrB10_D2_SafeSpot.lua
+++ Blish HUD/scripts/General/StrB10_D2_SafeSpot.lua
@@ -1,13 +1,20 @@
 HMP.StrB10_PrimordusSafeSpot = {
   marker = World:MarkerByGuid("jWwX9IzYeEWK/6ItALBYTw=="),
-  color = { I:Color(64, 255, 54, 255), I:Color(255, 54, 54, 255) },
+  center = I:Vector3(15.555, -554.495, 391.713),
+  color = { I:Color(64, 255, 54, 255), I:Color(255, 54, 54, 255), I:Color(255, 247, 0, 255) },
 }
 
 local SafeSpot, player = HMP.StrB10_PrimordusSafeSpot, Mumble.PlayerCharacter
 
 local function Tick_Primordus_SafeSpot(gameTime)
+  Debug:Watch("Vector", (player.Position - SafeSpot.center ):Length() )
+  
   if( ( player.Position - SafeSpot.marker.Position ):Length() > SafeSpot.marker.Size + 0.1) then
-    SafeSpot.marker.Tint = SafeSpot.color[1]
+    if( ( player.Position - SafeSpot.center ):Length() <= 14.44 ) then
+      SafeSpot.marker.Tint = SafeSpot.color[1]
+    else
+      SafeSpot.marker.Tint = SafeSpot.color[3]
+    end
   elseif( ( player.Position - SafeSpot.marker.Position ):Length() <= SafeSpot.marker.Size + 0.1) then
     SafeSpot.marker.Tint = SafeSpot.color[2]
   end
