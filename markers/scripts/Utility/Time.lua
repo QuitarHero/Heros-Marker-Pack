857c2b22ccd9b37079d86dc19711499d441099fb
Quentin <Quitar@ymail.com>
2025-06-28 18:09:56 -0500

    Updated GetUtcTime
    This breaks EoD & IBS tracking. Those need to be fixed.

--- markers/scripts/Utility/Time.lua
+++ markers/scripts/Utility/Time.lua
@@ -1,33 +1,12 @@
 HMPtime = {
-  GetUtcTime = function (interval, addTime)
-    if(addTime == nil) then addTime = 0 end
-    local LeapDayAmt = HMPtime.GetLeapDayAmt()
-    local currentTime = os.time( os.date("!*t") ) - addTime - (LeapDayAmt * 86400)
-    local totalDays = math.floor( ( currentTime / 86400) )
-    local remainder = math.fmod(totalDays, interval)
-    
-    --Debug:Print("Leap Year Testing: " .. LeapDayAmt)
+  --Gets the remainder of the total day amount since epoch and the provided interval.
+  --"minusTime" is optional
+  GetUtcTime = function (interval, minusTime)
+    if(minusTime == nil) then minusTime = 0 end
+    local time = os.time( os.date("!*t") ) - minusTime
+    local TotalDays = math.floor( (time / 86400) )
+    local remainder = math.fmod(TotalDays, interval)
     
     return remainder
-  end,
-  
-  GetLeapDayAmt = function()
-    local LeapDayAmt, YearAmt = 0, tonumber(os.date("%Y"))
-    
-    for i = YearAmt, 1970, -1 do
-      if( i ~= os.date("%Y") ) then
-        if( i % 400 == 0 or i % 4 == 0 and i % 100 ~= 0 ) then
-          LeapDayAmt = LeapDayAmt + 1
-        end
-      else
-        if( os.date("%m") > 2 ) then
-          if( i % 400 == 0 or i % 4 == 0 and i % 100 ~= 0 ) then
-            LeapDayAmt = LeapDayAmt + 1
-          end
-        end
-      end
-    end
-    
-    return LeapDayAmt
   end
 }
