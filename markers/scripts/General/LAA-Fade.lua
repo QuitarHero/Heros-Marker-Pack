cbad2af81f66c8467404026054e8a2c49dbfc367
Quentin <Quitar@ymail.com>
2026-02-04 18:20:50 -0600

    Removed Instance Support, Added W8 CM's

--- markers/scripts/General/LAA-Fade.lua
+++ markers/scripts/General/LAA-Fade.lua
@@ -1,80 +1,73 @@
 HMP.LAA_Fade = {
   -- (1) Spirit Vale
-  { -- Titles / Vale Guardian / Spirit Woods / Gorseval / Sabetha / Full Clear / Chests (inactive)
+  { -- Titles / Vale Guardian / Spirit Woods / Gorseval / Sabetha / Chests
     "xxDqXTXM+kmC7TdIrADQdQ==", "OF/Tf7csfkOhr+LnIPUVkA==",
-    "yO/CWPVMnkGUGFNOrtCfRw==", "XR1Vf8maAUGMn2yKP857OA==", "zEJzxPA1pUqYJxBQ6TXztQ==", "WfnFHlrOcUuWaCNhcvkpyQ==", "Waf/dgmWA0uhbIdsu4Wm4Q==",
-    "WCV5sAIak0iiHT/RSlw7+w==", "iBL4CkcAYkq4JwdWQU+RbA==", "zIgn4sdkuEKNdDHv6c3UwQ==", "BiyyH7fNUkyaSMfXAOyO3g==", "Q17n0zCOGU2990dkL4tQGg==",
-    "KHmEMgwKpEKcD7J4rCfY0Q==", "nKxNJfMSPkKlUU/3rC8/OA==", "Pi9lvkPVb0GpVLagTWPUBg==", "k4J+ROD+6UaQ2KN1STqJhQ==", "5eG2yJ5MKEGTRT9wtOzAIg==",
-    "ajte7FPIi0C+oYBUOYy21Q==", "uLbWeADq8U22UT45w9GHBw==", "gjQd73GsZk2EYBxhM6lUUg==", "O1ShbobG1Ueyj1aq3o+plg==", "NrCRF61b00G9MevabkDwaA==",
-    "FpWADMDn4UeboS1y8Y6cDQ==", "ayJduf/fn0imadz/sYaqQg==", "2W+D1px7dU+0fpvSze1SKQ==", "q9e6pFheSESC+uv0ptrCpQ==",
+    "yO/CWPVMnkGUGFNOrtCfRw==", "XR1Vf8maAUGMn2yKP857OA==", "zEJzxPA1pUqYJxBQ6TXztQ==", "WfnFHlrOcUuWaCNhcvkpyQ==",
+    "WCV5sAIak0iiHT/RSlw7+w==", "iBL4CkcAYkq4JwdWQU+RbA==", "zIgn4sdkuEKNdDHv6c3UwQ==", "BiyyH7fNUkyaSMfXAOyO3g==",
+    "KHmEMgwKpEKcD7J4rCfY0Q==", "nKxNJfMSPkKlUU/3rC8/OA==", "Pi9lvkPVb0GpVLagTWPUBg==", "k4J+ROD+6UaQ2KN1STqJhQ==",
+    "ajte7FPIi0C+oYBUOYy21Q==", "uLbWeADq8U22UT45w9GHBw==", "gjQd73GsZk2EYBxhM6lUUg==", "O1ShbobG1Ueyj1aq3o+plg==",
     "Z6iFGUDZv0+J9RE7y8ZOfg==", "c03BJl+KDk61YebWcz8OKA==", "IYMoQXUWdU2EcRgP1wXZ7w==", "eJyw2ggyDE+uAQuqPh02mA=="
   }, -- (2) Salvation Pass
-  { -- Titles / Slothasor / Bandit Trio / Matthias / Full Clear / Chests (inactive)
+  { -- Titles / Slothasor / Bandit Trio / Matthias / Chests
     "mdhEf+Xf30mxlZOSkqxRSA==", "NsnruMz4WEC8wn1BQPZGRw==",
-    "VQmq6kt1MUqXJLNkat4KWQ==", "eUFQ90EiVUu720MIruGvuQ==", "sy9JTDWFZUqJXS+H2CB0lA==", "nA3j7yAuZkiE12i2Ehaf+w==", "DV1xcNAIVkavRbx+wHfKwA==",
-    "8/0yDlxYqUqj9Gfgh5qCiQ==", "dkZNq+xa4Ua7bfHjTnqFOQ==", "Z+phLtrabEWop02DzQi5qg==", "SUC/AJ8wdUe21HAdod+W5A==", "4vChcpj/bEOJm7Q9B62ydw==",
-    "lWPzU3taHkKenJ/o4a0kYA==", "AUbGOzLHyEOXReUKSdujQQ==", "zbtU4A3lok6F+Ua301EaXA==", "3Wxm+ojuWkS+x8R0F3fVBg==", "dmQdp/OFkUC/4P8comCj8Q==",
-    "jWv/r0Rzu0ir8oZICqw15Q==", "nQX89K8ppEOGlrSTbXRxWg==", "BjVk/K+f2E6Te0lWbdWYiQ==", "t8THwSwOBUu4LcmRAxxNaA==",
+    "VQmq6kt1MUqXJLNkat4KWQ==", "eUFQ90EiVUu720MIruGvuQ==", "sy9JTDWFZUqJXS+H2CB0lA==", "nA3j7yAuZkiE12i2Ehaf+w==",
+    "8/0yDlxYqUqj9Gfgh5qCiQ==", "dkZNq+xa4Ua7bfHjTnqFOQ==", "Z+phLtrabEWop02DzQi5qg==", "SUC/AJ8wdUe21HAdod+W5A==",
+    "lWPzU3taHkKenJ/o4a0kYA==", "AUbGOzLHyEOXReUKSdujQQ==", "zbtU4A3lok6F+Ua301EaXA==", "3Wxm+ojuWkS+x8R0F3fVBg==",
     "zYsQ1C4kAkS6zhpjzVOSXg==", "r5tGkT/frkqdDjVrd/7hnQ==", "WDUJkqt300CdDDLfQzVK1Q==", "wcrPg/OPrUyJZZRUUegdJQ=="
   }, -- (3) Stronghold of the Faithfl
-  { -- Titles / Escort / Keep Construct / Twisted Castle / Xera / Full Clear / Chests (inactive) / Weekly CM Tags
+  { -- Titles / Escort / Keep Construct / Twisted Castle / Xera / Chests / Weekly CM Tags
     "jpbhO3Be10CGHJrcSC44qA==", "m9upIieYDUua1F1BoOpppw==",
-    "N6ehsVZAMkOQapeKWvUDSA==", "l/9Q56qPj0WLEbBQ6rIX4g==", "xSXSlyPNhEaplWC1Z5Fk2g==", "z+6hJBaJ0Ei5YlraWuwveg==", "+65bsTCmGkSkgwXz9ST9TA==",
-    "IDtWNKXAiU6ZRraOld/Okg==", "JpzoZOxm/kWHbk0NJPPHjA==", "Qxh2RFuhHkiFIgQgTEM6ZQ==", "MeQSJSIBR0yQN6bFjIj3VA==", "xfkwzX6+8UGx1F01djqPEg==",
-    "/YaienclP0CNHMCSpQ0UDw==", "HN9q4WYQk0a6+O8unzDVgw==", "bOoSfv302EKFuDuWiwox/Q==", "RjmksXrsOkKlRv09teYQHg==", "aVghe2h3nEKRWXOvtmANpA==",
-    "7TezsGNb3EaLZyzHv6Qggw==", "QBCKH33gFkyyGJ+yHj1otQ==", "p2pz/yt0PEGm7Lij73xZZQ==", "pI8G2BqGCk+0QJeuLkzaMQ==", "pJJpHeKjAkumZmYwvXT7zw==",
-    "mzd63Uf2YUC29rRZNl6vRA==", "lNUxEI29WEC8r9P6t8zcQA==", "6o4sLtuA5EWE89KjKnY31w==", "hFfPgD76kUO2Z8dokiMGXw==",
+    "N6ehsVZAMkOQapeKWvUDSA==", "l/9Q56qPj0WLEbBQ6rIX4g==", "xSXSlyPNhEaplWC1Z5Fk2g==", "z+6hJBaJ0Ei5YlraWuwveg==",
+    "IDtWNKXAiU6ZRraOld/Okg==", "JpzoZOxm/kWHbk0NJPPHjA==", "Qxh2RFuhHkiFIgQgTEM6ZQ==", "MeQSJSIBR0yQN6bFjIj3VA==",
+    "/YaienclP0CNHMCSpQ0UDw==", "HN9q4WYQk0a6+O8unzDVgw==", "bOoSfv302EKFuDuWiwox/Q==", "RjmksXrsOkKlRv09teYQHg==",
+    "7TezsGNb3EaLZyzHv6Qggw==", "QBCKH33gFkyyGJ+yHj1otQ==", "p2pz/yt0PEGm7Lij73xZZQ==", "pI8G2BqGCk+0QJeuLkzaMQ==",
     "QNA2x9bYMkWAhrqse5NASA==", "yNX+mGYAo0OL6zByStFJfg==", "k1vRSaPri0qFQ7ECNB7WXw==", "lz84TEW/FUyZiVlGTUxbbA==",
     "brW6Za7nO06RPzulHK3yvQ=="
   }, -- (4) Bastion of the Penitent
-  { -- Titles / Cairn / Mursaat Overseer / Samarog / Deimos / Full Clear / Chests (inactive) / Weekly CM Tags
+  { -- Titles / Cairn / Mursaat Overseer / Samarog / Deimos / Chests / Weekly CM Tags
     "FSd7f7j000SJzB8pIRE2aA==", "KX+/8158HUu8hGHdMfb6Qw==",
-    "yFyYbK1jcUeRwL7B2Z//bw==", "fJcHTJEs3kGA17blywu8Cg==", "bpJdH98OEk+6JJ2FGzBrnQ==", "tDZFZB1giEWjJiXO0F0G2g==", "EifcsHS9ik6VRV+zlhh4uw==",
-    "kaY4o7NXgki2J6CGgKaS+g==", "+mkTqF34OEiRfsEvyauHtg==", "qsk9A0K+B0WszoyzMaknkQ==", "USipnyDankGI88eBhHhjQA==", "OmPiTPf3mk21rKN+6Qz+8g==",
-    "Ne7+RrJiyE2HGuuE94iIuQ==", "xnT1VlnulkCKYLw0dOq4gA==", "GAb72lpCRUKaHp6tqFB8rQ==", "caTOeKhgyEWLDk0cOAoqvg==", "P6gChwNaGE2+fqfoObPqQA==",
-    "QkT8cFKnXky30R6jese+YQ==", "pQo72hXxiE+LuUB5WR0auA==", "hy81+ykLUUuMLStwPPF/3w==", "LwZnT5vNMEef84vHoLVDFg==", "FvZbgH7B4kuuQ/hLz8kWBQ==",
-    "5GV3TEWIF0+UiSIVxrJ4WQ==", "fq4HiUmzKEq7x1ZxC1f1Nw==", "66Bl9wBW8kOyZbsmfoPgiQ==", "WVmqCx8DUkirkqEYmIlaXg==",
+    "yFyYbK1jcUeRwL7B2Z//bw==", "fJcHTJEs3kGA17blywu8Cg==", "bpJdH98OEk+6JJ2FGzBrnQ==", "tDZFZB1giEWjJiXO0F0G2g==",
+    "kaY4o7NXgki2J6CGgKaS+g==", "+mkTqF34OEiRfsEvyauHtg==", "qsk9A0K+B0WszoyzMaknkQ==", "USipnyDankGI88eBhHhjQA==",
+    "Ne7+RrJiyE2HGuuE94iIuQ==", "xnT1VlnulkCKYLw0dOq4gA==", "GAb72lpCRUKaHp6tqFB8rQ==", "caTOeKhgyEWLDk0cOAoqvg==",
+    "QkT8cFKnXky30R6jese+YQ==", "pQo72hXxiE+LuUB5WR0auA==", "hy81+ykLUUuMLStwPPF/3w==", "LwZnT5vNMEef84vHoLVDFg==",
     "8VEzoX3YL06rUOAHQd03rg==", "i5NsFHhFikqs9wCzsN0vag==", "1xAc45X6DUivc13mWDk1Sw==", "FILrTw2Q1kSDryIrVmrCAA==",
     "63oTeepAiE6y8vujehqRog==", "+mjTVfP8/0OzdF0TEVCdUg==", "eXAIB9/J10KUoTpaZ4mSVQ==", "RmLCuvb68E2331iUDsxbMg=="
   }, -- (5) Hall of Chains
-  { -- Titles / Soulless Horror / River of Souls / Statues of Grenth / Dhuum / Full Clear / Chests (inactive) / Weekly CM Tags
+  { -- Titles / Soulless Horror / River of Souls / Statues of Grenth / Dhuum / Chests / Weekly CM Tags
     "HUhMw0PB0EqN13Z1yFTLyQ==", "IKwQjo0jzESpPHcanHt96w==",
-    "86cnNrgzRkOR0zLfN50wjw==", "VGJMxyLo9EeDzCtmJ0XEAA==", "qUFMzzHo3UugJzKD/qKO3w==", "RUEpfGHAAkG7LwVbMZ52OA==", "e2GZJnDY10KaiTU79ZKjFw==",
-    "IVcXI6T13kyMPK5Vqj4Bpg==", "uoJVzgYx+UGtdPMG/uSvcA==", "MaG8ytCNP0il4NpEIl3y8w==", "lz0/nXyueUulhMZ+w3o/MQ==", "XrdSPnDTJ0KT2yE/3oau6Q==",
-    "wvi4ddM5JEy/daodu9zwfw==", "onxbjQynWUuJENzmjZBLQQ==", "a6k3m1wGjkOPiTwCGQrvgQ==", "Z7Yfm0NmLk2IWxBglF7YRA==", "oPZtGin/KESP/ym8bwZkKQ==",
-    "0RuwmKQPV0qwt/28Y8p9hw==", "UHg2e/K4X0W+8H69pSS4uA==", "q3sz1HdybU29F4oGRNxDcQ==", "jfN/CUYXk0q/dElUCYqbfA==", "N/mYZ9YqaE26eM7mTNgW+A==",
-    "MGXubuVF3Eu2Htz57WsOpA==", "Zn6I7aa5PkC2gtdPjIqCyg==", "OKLomCthwUKgWXIJ83qXoQ==", "7eqvrj6zm0mLyQ5CqLCX3g==",
+    "86cnNrgzRkOR0zLfN50wjw==", "VGJMxyLo9EeDzCtmJ0XEAA==", "qUFMzzHo3UugJzKD/qKO3w==", "RUEpfGHAAkG7LwVbMZ52OA==",
+    "IVcXI6T13kyMPK5Vqj4Bpg==", "uoJVzgYx+UGtdPMG/uSvcA==", "MaG8ytCNP0il4NpEIl3y8w==", "lz0/nXyueUulhMZ+w3o/MQ==",
+    "wvi4ddM5JEy/daodu9zwfw==", "onxbjQynWUuJENzmjZBLQQ==", "a6k3m1wGjkOPiTwCGQrvgQ==", "Z7Yfm0NmLk2IWxBglF7YRA==",
+    "0RuwmKQPV0qwt/28Y8p9hw==", "UHg2e/K4X0W+8H69pSS4uA==", "q3sz1HdybU29F4oGRNxDcQ==", "jfN/CUYXk0q/dElUCYqbfA==",
     "JSsAJSv4yE62XDQuatBYNw==", "LGojo1RVUEOVAN4SEK66LA==", "7WglVIKNXEGA3qPDsK4ndQ==", "jiStgW0aHEafszRGkJxPHA==",
     "OfkmkHaM+0eJXsD7Z8vMIg==", "j2l1LQpRtUGiWVAbZvftyA=="
   }, -- (6) Mythwright Gambit
-  { -- Titles / Conjured Amalgamate / Twin Largos / Qadim / Full Clear / Chests (inactive) / Weekly CM Tags
+  { -- Titles / Conjured Amalgamate / Twin Largos / Qadim / Chests / Weekly CM Tags
     "5PS5QzCDa0y4mHdlwsb9dw==", "6WIRCtjB90imU1y6QSWang==", "DpbZ61s3rECdYVslcrYx4Q==",
-    "y825N6C8vU2k/joYg700vQ==", "cWxoCwmCYkyra/R+bpFVrw==", "a/aXprjd+U2W2BQhP6EJlQ==", "JIQmoQXMC0uzD2XeQvso1w==", "jpe2gXAj3Eam6dQwpxBOfw==",
-    "28oldJi7Fk246z/LNjiPXA==", "+Uq+dggHO06VYy7sKulm+Q==", "Yy2IVju8RESckkIaEnSyGw==", "9y2azns8gEKbvg/e0CAiqQ==", "5fNiEuxXfkqXWI3HyB+cTQ==",
-    "JXMfPEdERUequjhusleiPg==", "PDyOr6itt0iUUB3ZEqhm5w==", "FKVPp+ECREy2PKy0pDXfqA==", "ZbE291TbQkqEiCdinwRz5Q==", "bjzjJuhgFEW9NbW4uCEdig==",
-    "50e11k6OqE2SoeujO+3LeA==", "Jd9Sbq3mwEKvaiJgw/bfYg==", "F+sqpNWtq0eXIQtbFLi03w==", "kjPRhjqrPUCgsgQEDX/dBw==",
+    "y825N6C8vU2k/joYg700vQ==", "cWxoCwmCYkyra/R+bpFVrw==", "a/aXprjd+U2W2BQhP6EJlQ==", "JIQmoQXMC0uzD2XeQvso1w==",
+    "28oldJi7Fk246z/LNjiPXA==", "+Uq+dggHO06VYy7sKulm+Q==", "Yy2IVju8RESckkIaEnSyGw==", "9y2azns8gEKbvg/e0CAiqQ==",
+    "JXMfPEdERUequjhusleiPg==", "PDyOr6itt0iUUB3ZEqhm5w==", "FKVPp+ECREy2PKy0pDXfqA==", "ZbE291TbQkqEiCdinwRz5Q==",
     "ocrS+vz27UWRkT44DZvyHQ==", "PHHj2aXTPE2hE0yfq2DKMw==", "KAUnGDuRRkqUoo+rrw5Saw==",
     "mqdJ9FGI0E+xzvPF4TMyGw==", "xYrQXhqswU6T/MBp7TploA==", "rwuTuj+XQkWgHkw10Ddw0g=="
   }, -- (7) The Key of Ahdashim
-  { -- Titles / Gate / Cardinal Adina / Cardinal Sabir / Qadim the Peerless / Full Clear / Chests (inactive) / Weekly CM Tags
+  { -- Titles / Gate / Cardinal Adina / Cardinal Sabir / Qadim the Peerless / Weekly CM Tags
     "j6ZEEP0JUUKCYUYkDM363Q==", "28wSDARdakCZ07bJ4Z7Uiw==",
-    "faOHRVHZg0+fHEd4rbcTzw==", "GXYoqFRU5kGPBjNV4lJdlA==", "c2vE59kCREGD5dr3bKKUMA==", "gJKR091RlUasil9kgNcWJA==", "zAMKIvsJZESr0QJdtMHBfQ==",
-    "y+43mi2qN0yBVE7NKJCklQ==", "XHMjsJsC5E2I7YWyJL/3vw==", "khX4y56I5EqRXQ4YEHShCQ==", "t9BqwrxO+E2NnwW7SqQG3Q==", "TKjDUiZk50qTjwRNf1V0iQ==",
-    "9tBh5N04v0eR9H6bdzPrUQ==", "SQ4hJbNvokadHdrMw4gh8Q==", "P89mYyQcbEi849T7kUIrsw==", "kylG4HBb+0Wr9uNnOJc/Sw==", "EEVZqUJZ80S1mW1fwIrLTw==",
-    "tVsrnBnHKkWlsRTSkQKq4A==", "aJvDffbyl0mgBMu3qNbBFw==", "Q6N03WNWrEG9HdYuXcqncg==", "P/u3l2qkl0a+WtW7bnKwtQ==", "01kKt+/upEyGXFjq8/3akg==",
-    "pAdRQLf7WkGFMkd2Q5lC5g==", "v9uuOb9rq0qD1GvQUOmOvQ==", "SPgkBZKtrkGUBHunQy5+8w==", "g2y4D7BSN0SPbyHEdgbMKg==",
+    "faOHRVHZg0+fHEd4rbcTzw==", "GXYoqFRU5kGPBjNV4lJdlA==", "c2vE59kCREGD5dr3bKKUMA==", "gJKR091RlUasil9kgNcWJA==",
+    "y+43mi2qN0yBVE7NKJCklQ==", "XHMjsJsC5E2I7YWyJL/3vw==", "khX4y56I5EqRXQ4YEHShCQ==", "t9BqwrxO+E2NnwW7SqQG3Q==",
+    "9tBh5N04v0eR9H6bdzPrUQ==", "SQ4hJbNvokadHdrMw4gh8Q==", "P89mYyQcbEi849T7kUIrsw==", "kylG4HBb+0Wr9uNnOJc/Sw==",
+    "tVsrnBnHKkWlsRTSkQKq4A==", "aJvDffbyl0mgBMu3qNbBFw==", "Q6N03WNWrEG9HdYuXcqncg==", "P/u3l2qkl0a+WtW7bnKwtQ==",
     "nn5Ed6cLokqPrpKr2CFkdA==", "ncE1YBSs/ESj3VWnrGsuZg==", "IvNiK6vpBkOcyPPyFvX9hg==",
   }, -- (8) Mount Balrior
   { -- Titles / Ruined Camp / Decima / Greer / Ura / Full Clear
     "LVyXSMMXFEmzH/keHWc1Dg==", "aYLAysVMAECISV4JYKyizg==",
-    "ie67a5i5c0OFA61zwjLBIQ==", "1pxxqshQQkyDalMJs8vyzw==", "oFa42LZbt0mskCrKCXM1zw==", "WY/v4GIL5UKYbZe8yj5NWg==", "B4I8hc3QckyNVLD4X+T5BQ==",
-    "RPQ8/5i2vk2kDBFBZC628A==", "G7UScS49DEmdlH1eEqCMfg==", "btyBhXI+VEWOcqniDbPXZw==", "EyreTyG5AU6eXwcsKaKn9g==", "S0obZSwQQ0qoqtFtC+Ht9g==",
-    "GTh+InpVMEunsMKNljYfxw==", "tFqp7En/+Uqb5oYuBF07Hw==", "xgjJGocey0OQRmF+vCxV9A==", "Pn4sbL6550WjPaGG6B1e0A==", "Q9ySKePOUkWkh0oU1pYNGg==",
-    "jy59GMvChkS0udXv1SnZbQ==", "zx2IhdGJ8U25yDJghRX9hg==", "I5/20oSylECtK/gWvSMvXA==", "XXLRDsD7rkavbDogPkVumA==", "cXfp0tSSo0S8kjQ+lVwVWA==",
-    "pKYkP+W3LkOXgnL0rSGI6w==", "dBOI9jTbj06gT7pJUx6KGQ==", "0xRANu+RqUirQZjVBuCFaA==", "CBP32EfWP0CI3y8KKp+2/w==",
+    "ie67a5i5c0OFA61zwjLBIQ==", "1pxxqshQQkyDalMJs8vyzw==", "oFa42LZbt0mskCrKCXM1zw==", "WY/v4GIL5UKYbZe8yj5NWg==",
+    "RPQ8/5i2vk2kDBFBZC628A==", "G7UScS49DEmdlH1eEqCMfg==", "btyBhXI+VEWOcqniDbPXZw==", "EyreTyG5AU6eXwcsKaKn9g==",
+    "GTh+InpVMEunsMKNljYfxw==", "tFqp7En/+Uqb5oYuBF07Hw==", "xgjJGocey0OQRmF+vCxV9A==", "Pn4sbL6550WjPaGG6B1e0A==",
+    "jy59GMvChkS0udXv1SnZbQ==", "zx2IhdGJ8U25yDJghRX9hg==", "I5/20oSylECtK/gWvSMvXA==", "XXLRDsD7rkavbDogPkVumA==",
+    "5DqAe0mMHUGKvt+vXmYjoQ==", "yY+jAumGRkmYK9wFUnbybg==", "2RAolWaGJ0S1t8iYRR0y2w=="
   }
 }
-HMP.LAA_Trigger = { -- Wings 1 / 2 / 3 / 4 / 5 / 6 / 7
+HMP.LAA_Trigger = { -- Wings 1 / 2 / 3 / 4 / 5 / 6 / 7 / 8
   "tuacr+Gv9UiLskdieMqAvw==", "5HcLprv7mUmqFhNvSHhBnA==", "ELxXulJmCE6O4GPh10/zMA==", "lbslvR+bEUSgUEHN2TFhIg==", "xxUWfcVQA06WCiou6qVnFw==", "6ZfzJPriqEuUB2ZX2wqTtA==", "9ySCoUBWeU2GkeZD8j1nRg==", "MlsjvBaykEe4k8QMBgye1A=="
 }
 
